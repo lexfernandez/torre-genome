80b2e000f814f600eee00232f504e41bfd81c384
lexfernandez <olorenzo@outlook.com>
2020-12-27 13:41:39 -0600

    Sort skills and set space between tags

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Tag, Row, Col} from 'antd';
+import classes from './Skills.module.css';
+
 export const Skills = ({skills, editable, handleDelete}) => {
-  let tags = skills.map((skill) => (
-    <Tag
-      key={skill.id}
-      closable={editable}
-      onClose={() => handleDelete(skill)}
-      onClick={() => alert(skill.name)}>
-      {skill.name}
-    </Tag>
-  ));
+  let tags = skills
+    .sort((first, second) => {
+      if (first.name.length > second.name.length) return 1;
+      if (first.name.length < second.name.length) return -1;
+      return 0;
+    })
+    .map((skill) => (
+      <Tag
+        key={skill.id}
+        closable={editable}
+        onClose={() => handleDelete(skill)}
+        onClick={() => alert(skill.name)}>
+        {skill.name}
+      </Tag>
+    ));
 
   return (
     <Row>
-      <Col span={24}>{tags}</Col>
+      <Col span={24} className={classes.Tags}>
+        {tags}
+      </Col>
     </Row>
   );
 };
@@ -28,8 +38,8 @@ Skills.propTypes = {
       code: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
       additionalInfo: PropTypes.string,
-      weight: PropTypes.number.isRequired,
-      recommendations: PropTypes.number.isRequired,
+      weight: PropTypes.number,
+      recommendations: PropTypes.number,
       created: PropTypes.string.isRequired,
     }).isRequired,
   ).isRequired,
