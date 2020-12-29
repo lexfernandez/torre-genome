e99933068303353836b27a772b48048fe1e89300
lexfernandez <olorenzo@outlook.com>
2020-12-28 21:54:49 -0600

    Add api request to retrieve bio info

--- src/components/genome/Genome.js
+++ src/components/genome/Genome.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Col, Row, Skeleton} from 'antd';
 import classes from './Genome.module.css';
 import {Profile} from '../profile/Profile';
-import genome from './genome.json';
+import fakeGenome from './genome.json';
 import Behavior from '../behavior';
+import {useParams} from 'react-router-dom';
+import * as Api from '../../api/genome-api';
 
 export const Genome = () => {
+  const [genome, setGenome] = useState(undefined);
+  let {username} = useParams();
+
+  useEffect(async () => {
+    setGenome(undefined);
+    async function getData() {
+      try {
+        let response = await Api.getGenome(username);
+        console.log(JSON.stringify(response.data));
+        let bio = response.data;
+        console.log(bio);
+        setGenome(bio);
+      } catch (error) {
+        setGenome(fakeGenome);
+        console.log(error);
+      }
+    }
+    getData();
+  }, [username]);
+
   let profile = <Skeleton active paragraph={{rows: 5}} />;
   if (genome) {
     profile = (
-            <Profile
-              person={genome.person}
-              strengths={genome.strengths}
-              interests={genome.interests}
-              opportunities={genome.opportunities}
-              languages={genome.languages}
-            />
+      <Profile
+        person={genome.person}
+        strengths={genome.strengths}
+        interests={genome.interests}
+        opportunities={genome.opportunities}
+        languages={genome.languages}
+      />
     );
   }
 
