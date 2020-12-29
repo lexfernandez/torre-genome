0ffa9735a512b7fa2299be2fe1e11ebb5f56ee21
lexfernandez <olorenzo@outlook.com>
2020-12-28 21:34:23 -0600

    Add axios and set base url

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/app';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 import './index.css';
+axios.defaults.baseURL = 'https://torre.bio/api';
 
 ReactDOM.render(
   <React.StrictMode>
-          <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
